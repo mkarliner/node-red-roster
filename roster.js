2c6f75ba85319259332122f6ee85ccb0871bda87
Michael Karliner <mike@modern-industry.com>
2016-01-01 18:40:00 +0000

    FIXES for deletion. Mark active/inactive rather than delete

--- roster.js
+++ roster.js
@@ -13,46 +13,52 @@ module.exports = function(RED) {
 		this.indexProperty = config.indexProperty;
 		this.timeout = config.timeout;
         var node = this;
-        var roster  = {};
+        console.log("TJIS ", this)
+        this.roster = {};
+
         this.on('input', function(msg) {
 			var newEntry = true;
 			var payload = JSON.parse(msg.payload);
 			payload.rosterTimestamp = new Date();
-			console.log("ROS: ", payload, roster)
+            payload.rosterStatus = "active";
+			//console.log("ROS: ", payload, roster)
 			//Check for valid message
 			if(!payload[node.indexProperty]) {
 				node.error("Missing property " + node.indexProperty + " in incoming message", msg);
 				return;
 			}
 			// Have we already registered this entry?
-			if (roster[payload[node.indexProperty]]) {
+			if (this.roster[payload[node.indexProperty]]) {
 				newEntry = false; 
 			}
-            roster[payload[this.indexProperty]] = payload;
+            this.roster[payload[this.indexProperty]] = payload;
 
 			// Only send roster if it has changed.
 			if(newEntry == true) {
-	            node.send({payload: JSON.stringify(objarray(roster)) });
+	            node.send({payload: JSON.stringify(objarray(this.roster)) });
 			}
         });
-		setInterval(function(){
-			var entryDeleted = false;
-			for(var propertyName in roster) {
-				var entry = roster[propertyName];
-				var outputRoster = [];
-				var now = new Date();
-				var idle = (now-entry.rosterTimestamp)/1000;
-				if(entry.rosterTimestamp>0 && (idle > node.timeout)) {
-					delete roster[propertyName];
-					entryDeleted = true;
-				} 
-				outputRoster.push[roster[propertyName]];
-			}
-			// Only send roster if it has changed.
-			if(entryDeleted == true) {
-	            node.send({payload: JSON.stringify(objarray(outputRoster)) });
-			}
-		}, 1000)
+        function checkEntries(){
+        			var entryDeleted = false;
+        			for(var propertyName in this.roster) {
+        				var entry = this.roster[propertyName];
+        				var now = new Date();
+        				var idle = (now-entry.rosterTimestamp)/1000;
+                        //node.log("SI: ", entry, idle, propertyName)
+        				if(entry.rosterTimestamp>0 && (idle > node.timeout)) {
+        					//delete this.roster[propertyName];
+                            this.roster[propertyName].rosterStatus = "inactive";
+                            console.log("DELETED ", propertyName)
+        					entryDeleted = true;
+        				} 
+        				//this.roster.push[this.roster[propertyName]];
+        			}
+        			// Only send roster if it has changed.
+        			if(entryDeleted == true) {
+        	            node.send({payload: JSON.stringify(objarray(this.roster)) });
+        			}
+        		}
+		setInterval(checkEntries.bind(this), 1000)
     }
     RED.nodes.registerType("roster",Roster);
 }
